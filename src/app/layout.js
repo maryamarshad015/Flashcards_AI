c1d3db590ede27ea63aaa8c9e62d87ca538bc3d4
maryamarshad015@gmail.com <your.email@example.com>
2024-08-30 03:49:49 +0500

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider  publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    apiKey={process.env.CLERK_SECRET_KEY}
+  >
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
